a231a349e4ba89e6f2f304a4fd55e91a1a9c44ba
Lucas Moreira <moreirapontocom@gmail.com>
2019-09-11 11:13:42 -0300

    Inserting into mongo

--- server.js
+++ server.js
@@ -4,9 +4,35 @@ var express = require('express'),
 
 var app = express();
 
+var db = new mongodb.Db(
+    'instagram',
+    new mongodb.Server('mongo', 27017, {}),
+    {}
+);
+
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 app.listen(4200, function() {
     console.log('Servidor online na porta 4200');
+});
+
+
+
+
+app.get('/', function(req, res) {
+    res.send({ msg: 'olá' });
+});
+
+app.post('/api', function(req, res) {
+    var formData = req.body;
+
+    db.open(function(err, client) {
+        client.collection('postagens', function(err, collection) {
+            collection.insert(formData, function(err, result) {
+                res.json({ status: 'ok' });
+                client.close();
+            });
+        });
+    });
 });
